8ffd33086903f28af0e3129ec92d4b8979a07623
N1ckon6 <denis.nickon2@gmail.com>
2025-03-10 17:59:45 +0100

    Add benchmark data and update example.py

--- data/example.py
+++ data/example.py
@@ -1,12 +1,76 @@
 import sys
 
+def insertion_sort(data):
+    for j in range(len(data) - 1):
+        for i in range(len(data) - j - 1):
+            if(data[i] > data[i+1]):
+                data[i], data[i+1] = data[i+1], data[i]
+
+def shell_sort(data):
+    k = len(data) // 2
+    while(k > 0):
+        for i in range(k):
+            temporary_list = []
+            element_number = 0
+            for j in range(i, len(data), k):
+                temporary_list.append(data[j])
+            insertion_sort(temporary_list)
+            for n in range(i, len(data), k):
+                data[n] = temporary_list[element_number]
+                element_number += 1
+        k = k // 2
+
+def selection_sort(data):
+    for j in range(len(data) - 1):
+        min = j
+        for i in range(j + 1, len(data)):
+            if data[i] < data[min]:
+                min = i
+        data[j], data[min] = data[min], data[j]
+
+def heap_sort(data):
+    def heapify(data, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and data[i] < data[left]:
+            largest = left
+
+        if right < n and data[largest] < data[right]:
+            largest = right
+
+        if largest != i:
+            data[i], data[largest] = data[largest], data[i]
+            heapify(data, n, largest)
+
+    n = len(data)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(data, n, i)
+
+    for i in range(n - 1, 0, -1):
+        data[i], data[0] = data[0], data[i]
+        heapify(data, i, 0)
+
 def sort_using_algorithm(data, algorithm):
     # This function takes the algorithm identifier as input
     # However, it always uses the sorted function in Python
 
-    sorted_data = sorted(data)
+    if algorithm == 1:
+        insertion_sort(data)
+    elif algorithm == 2:
+        shell_sort(data)
+    elif algorithm == 3:
+        selection_sort(data)
+    elif algorithm == 4:
+        heap_sort(data)
+    elif algorithm == 5:
+        shell_sort(data)
+    elif algorithm == 6:
+        shell_sort(data)
 
-    return sorted_data
+    return data
 
 def main():
     # Command-line arguments: python script.py --algorithm <algorithm_number>
@@ -19,7 +83,7 @@ def main():
     # Read input data from standard input until the end of file (EOF)
     input=sys.stdin.read().split()
     try:
-        data = [int(x) for x in input[1:]]
+        data = [int(x) for x in input]
     except EOFError:
         print("Error reading input.")
 
